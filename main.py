5b4009019a5754808e799022864462bb25bf003d
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-10-07 17:34:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import telebot
 import config
+import neural
 import subprocess
 import os
 import sys
@@ -9,11 +10,17 @@ sys.setdefaultencoding('utf8')
 
 bot = telebot.TeleBot(config.TOKEN)
 
+@bot.message_handler(commands=['start'])
+def kolya(message):
+  bot.send_message(message.chat.id, 'Hello 😊, I am not a bot 🤖.')
+  bot.send_message(message.chat.id, '😌 Ask me everything you want.')
+  bot.send_message(message.chat.id, '❔ I will answer every your question.')
+
 @bot.message_handler(content_types=['text'])
 def lalala(message):
   if (message.text == 'Hello'):
     bot.send_message(message.chat.id, 'Hello Man')
   else:
-    u = 'Your message was: '+str(message.text)+' :-)'
-    bot.send_message(message.chat.id, u)
+    loln = add_phrase(message.text)
+    bot.send_message(message.chat.id, loln)
 bot.polling(none_stop=True)
