627f38c47b63fbdc70502ab35bde6c4ab6a24845
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-10-07 17:29:51 +0300

    Update neural.py

--- neural.py
+++ neural.py
@@ -1,1 +1,16 @@
+import os
+import random
 
+def get_reply():
+  alltext = fopen("allfiles.txt", "w").read()
+  masse = alltext.split('\n')
+  lengths = len(masse)
+  allleng = lengths-1
+  randomn = random.randint(0, allleng)
+  random = masse[randomn]
+  
+def add_phrase(phrase):
+  alltext = fopen("allfiles.txt", "w").read()
+  allsd = 'echo "'+phrase+'">>allfiles.txt'
+  os.system(allsd)
+  return get_reply()
